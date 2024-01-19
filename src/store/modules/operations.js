8a68b6417c8dba491c279ee5c4c0671e4c14577f
usssagn1 <zaytsevdima031107@gmail.com>
2024-01-19 12:53:58 +0200

    business logic / logic app

--- src/store/modules/operations.js
+++ src/store/modules/operations.js
@@ -2,11 +2,24 @@ const prefix = 'operations'
 
 export const operations = {
     state: () => ({
+        income: 0,
+        consumption: 0,
+        result: 0,
         data: []
     }),
     mutations: {
         setData(state, payload) { //
             state.data = payload
+
+            const income = payload.filter(({ amount }) => amount > 0)     // в общем, создаються массив объектов, в котором income - числа больше нуля,
+            income.forEach(e => {
+                state.income += e.amount
+            })
+            const consumption = payload.filter(({ amount }) => amount < 0) // а consuption - меньше
+            consumption.forEach(e => {
+                state.consumption += e.amount
+            })
+            state.result = new Intl.NumberFormat('eu-US').format(state.income - state.consumption)
         }
     },
     getters: {
--- src/views/v-dashboard.vue
+++ src/views/v-dashboard.vue
@@ -1,61 +1,42 @@
 <script setup>
 import {useStore} from 'vuex'
 import Chart from "primevue/chart";
-import {computed, onMounted, reactive, ref, watch} from "vue";
+import {computed} from "vue";
 
 const store = useStore()
-const data = computed(() => store.state.operations.data)
-const result = ref(0)
-const state = reactive({
-  income: 0,
-  consuption: 0
-})
-
-function loadingData (newValue) {
-  if(newValue.length) {
-    const income = newValue.filter(({ amount }) => amount > 0)     // в общем, создаються массив объектов, в котором income - числа больше нуля,
-    income.forEach(e => {
-      state.income += e.amount
-    })
-    const consuption = newValue.filter(({ amount }) => amount < 0) // а consuption - меньше
-    consuption.forEach(e => {
-      state.consuption += e.amount
-    })
-    result.value = new Intl.NumberFormat('eu-US').format(state.income - state.consuption)
-  }
-}
-onMounted(() => {
-  loadingData(data.value)
-})
-
-
-
-const chartData = reactive({
-  labels: ['Income ', 'Expense'],
-  datasets: [
-    {
-      data: [computed(() => state.income), computed(() => state.consuption)],
-      backgroundColor: ['#2DB370', '#73D8FF']
-    }
-  ]
-})
-const lightOptions = reactive({
-  plugins: {
-    legend: {
-      labels: {
-        color: '#FFFFFF'
+const state = computed(() => store.state.operations)
+const chart = computed(() => {
+  return {
+    chartData: {
+      labels: ['Income ', 'Expense'],
+      datasets: [
+        {
+          data: [state.value.income, state.value.consumption],
+          backgroundColor: ['#2DB370', '#73D8FF']
+        }
+      ]
+    },
+    lightOptions: {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#FFFFFF'
+          }
+        }
       }
     }
+
   }
 })
+
 
 </script>
 
 <template>
   <div>
     <div :class="$style.chart">
-      <Chart  type="doughnut" :data="chartData" :options="lightOptions"/>
-      {{ result }}
+      <Chart  type="doughnut" :data="chart.chartData" :options="chart.lightOptions"/>
+      {{ state.result }}
     </div>
   </div>
 </template>
